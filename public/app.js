66c3b7e1a28c376be358e007b3393ad0ee41746b
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-05-27 15:46:49 +0300

    add comments

--- public/app.js
+++ public/app.js
@@ -1,17 +1,24 @@
+// продукт с номером id добавляется в корзину
 function add_to_cart(id) {
 
+	// формируем ключ для веб хранилища
 	id='prod_'+id;
+	// получам информацию (сколько уже продуктов с данным id находится в корзине) 
+	// из веб хранилища по ключу
 	var x=localStorage.getItem(id);
 	x=+x+1;
+	// записываем информацию в веб хранилище
 	localStorage.setItem(id, x);
 	alert("You add sushi with ID #"+id+ " - "+x);
 
 }
 
+// сколько продуктов находится в корзине
 function how_items_to_cart() {
 
 	var sum=0;
 
+	// проходим по всем ключам хранилища, суммируя их значения
 	for ( var i = 0; i < localStorage.length; ++i ) {
   		sum = sum+localStorage.getItem(localStorage.key( i ))*1;
 	}
